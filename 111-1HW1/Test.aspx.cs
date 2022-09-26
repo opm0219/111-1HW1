6ec944ef6096698161e68186051321505eb00f9b
opm0219 <opm0219@gmail.com>
2022-09-26 11:03:31 +0800

    ghjkl

---
+++ 111-1HW1/Test.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1HW1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("Hello App");        }
+
+        protected System.Void Button1_Click()
+        {
+            Response.Write("Hello Button");
+        }
+    }
+}
